e8eed4f27f2236cd6f2e985e98cc771a595dfd5e
Ben Bridges <ben2d2@gmail.com>
2012-09-26 17:28:39 -0500

    Still working on it, feedback is appreciated

--- lib/cal.rb
+++ lib/cal.rb
@@ -1,7 +1,8 @@
 class Calendar
 
-	def initialize(year)
-		@year=year
+	def initialize(month, year)
+		@year=year.to_i
+		@month=month.to_i
 	end
 
 	FOUR_YRS = 4
@@ -14,45 +15,85 @@ class Calendar
 		elsif @year % ONE_HUNDRED_YRS == 0
 			false
 		elsif @year % FOUR_HUNDRED_YRS == 0
-			false
+			true
 		else
 			false
 		end
 	end
-
-	day_of_month=1
-
-	def march_offset(month)
-		((month+1)*26/10).floor
+	
+	def day_of_week(month, year)
+		if month == 1||month == 2
+			month += 12
+			year -= 1
+		end
+		leap_year_offset = (year/4).floor+6*(year/100).floor+(year/400).floor
+		march_offset = ((month+1)*26/10).floor
+		(1+march_offset+year+leap_year_offset)%7
 	end
 
-	def leap_year_offset(year)
-		(year/4).floor+6*(year/100).floor+(year/400).floor
-	end
 
-	def day_of_week(month, day_of_month, year)
-		(day_of_month+march_offset(month)+year+leap_year_offset(year))%7
-	end
 
-	def days_in_month(month, year)#calls number of arguments for test line #13 in test file
+
+	def days_in_month#calls number of arguments for test line #13 in test file
 		#determine how many days per month - uses is_leap_year to count for Feb
-		if is_leap_year? && month == 2 
+		if is_leap_year? && @month == 2 || @month == 14
 			days_range = (1..29).to_a
-		elsif is_leap_year? && month == 2
+		elsif @month == 2 || @month == 14
 			days_range = (1..28).to_a
-		elsif month == 4||month == 6||month == 9||month == 11
+		elsif @month == 4||@month == 6||@month == 9||@month == 11
 			days_range = (1..30).to_a
 		else
 			days_range = (1..31).to_a
 		end	
-		days_range.length
+		blankstr=""
+		i=0
+		j=0
+		days_range.each {|m|
+			if m.to_s.length<2
+			digit_space=" "
+			else
+			digit_space=""
+			end
+
+		zellers=day_of_week(@month, @year)
+		if zellers == 0
+			zellers = 6
+		else
+			zellers=zellers-1
+		end
+		offset_zel=5-zellers
+		if i==offset_zel
+		start="   "*zellers
+
+		else
+			
+		start=""
+		end
+
+			if j == 6-zellers+7||j == 6-zellers+14||j == 6-zellers+21||j == 6-zellers+28||j==offset_zel+1
+				line_break="\n"
+			else
+				line_break=""
+			end
+			charac=digit_space, m
+			print start, charac,  " ", line_break
+		i+=1
+		j+=1
+	}
+		blankstr
+		
 	end
 	def month_name(month)
 		month_nm=["January","February","March","April","May","June","July","August","September","October","November","December"]
 		month=month_nm[month]
 	end
  
-	def generate_cal(month, year)	
+	def generate_cal(month, year)
+		month=month.to_i
+		year=year.to_i
+		puts month_name(month-1), year
+		puts "Su Mo Tu We Th Fr Sa"
+		print @buffer, days_in_month
 
 	end
 end
--- test/test_cal.rb
+++ test/test_cal.rb
@@ -5,52 +5,37 @@ class CalendarTest < Test::Unit::TestCase
 
 
 	def test01_is_leap_year
-		cal = Calendar.new(2001)
-        assert_equal(false, cal.is_leap_year?)
+		cal = Calendar.new(2, 2012)
+        assert_equal(true, cal.is_leap_year?)
     end
 
     def test02_no_of_days_in_month
-    	cal = Calendar.new(2012)
-    	assert_equal(29, cal.days_in_month(2, 2012))
+    	cal = Calendar.new(2, 2011)
+    	assert_equal(28, cal.days_in_month)
     end
 
     def test03_no_of_days_in_month_range_length
-        cal = Calendar.new(2012)
-        assert_equal(29, cal.days_in_month(2, 2012))
-    end
-
-    def test04_march_offset
-    	cal = Calendar.new(2012)
-    	assert_equal(36, cal.march_offset(13))
-    end
-
-    def test05_leap_year_offset
-        cal = Calendar.new(2012)
-        assert_equal(628, cal.leap_year_offset(2012))
-    end
-
-    def test06_leap_year_offset
-        cal = Calendar.new(1955)
-        assert_equal(606, cal.leap_year_offset(1955))
+        cal = Calendar.new(2, 2012)
+        assert_equal(29, cal.days_in_month)
     end
 
     def test07_day_of_week
-        cal = Calendar.new(1835)
-        assert_equal(4, cal.day_of_week(7, 1, 1835))
+        cal = Calendar.new(13, 1835)
+        assert_equal(6, cal.day_of_week(13, 1835))
     end
 
     def test08_day_of_week
-        cal = Calendar.new(1955)
-        assert_equal(4, cal.day_of_week(6, 1, 1955))
+        cal = Calendar.new(6, 1955)
+        assert_equal(4, cal.day_of_week(6, 1955))
     end
 
     def test09_month_name
-        cal = Calendar.new(2012)
+        cal = Calendar.new(3, 2012)
         assert_equal("March", cal.month_name(2))
     end
 
     def test10_month_name
-        cal = Calendar.new(2012)
+        cal = Calendar.new(9, 2012)
         assert_equal("October", cal.month_name(9))
     end
 end
